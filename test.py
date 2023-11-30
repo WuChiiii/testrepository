725ee43b1a0b65ada448fc386dd874d4f3691e15
bruno <chichun920718@gmail.com>
2023-11-30 22:56:47 +0800

    month_success

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 import time
 # 创建一个浏览器实例
 driver = webdriver.Chrome()
@@ -12,7 +13,7 @@ button = driver.find_element(By.XPATH, button_xpath)
 
 # 模拟点击按钮
 button.click()
-time.sleep(5)
+time.sleep(2)
 # 定位图标元素
 text_to_match = "宜蘭"
 xpath_td = f"//td[div[text()='{text_to_match}']]"
@@ -23,21 +24,21 @@ ixpath = f"{xpath_td}/following-sibling::td//i[@class='fas fa-chart-line cursor-
 icon_element = driver.find_element(By.XPATH, ixpath)
 # //*[@id="station_table"]/table/tbody/tr[3]/td[6]/div
 icon_element.click()
-time.sleep(5)
+time.sleep(1)
 month_button_xpath = "//*[@id='main_content']/section[2]/div/div/aside/div/div[5]/img"
 month_button = driver.find_element(By.XPATH, month_button_xpath)
 
 # 模拟点击按钮
 month_button.click()
-time.sleep(5)
-month1_button_xpath = "//*[@id='main_content']/section[2]/div/div/section/div[6]/div[1]/div[1]/label/div/div[2]"
-month1_button = driver.find_element(By.XPATH, month1_button_xpath)
+time.sleep(1)
+date_input_xpath = "//*[@id='main_content']/section[2]/div/div/section/div[6]/div[1]/div[1]/label/div/div[2]/input"
+date_input = driver.find_element(By.XPATH, date_input_xpath)
 
-# 模拟点击按钮
-month1_button.click()
-time.sleep(5)
-# 點擊<i>標籤
-# 现在您可以继续提取视窗中的内容或执行其他操作
+# 使用 JavaScript 设置输入框的值
+new_date_value = "2022/01"
+driver.execute_script("arguments[0].value = arguments[1]", date_input, new_date_value)
+# 等待一段时间（可選）
+time.sleep(1)
 
 # 关闭浏览器
-# driver.quit()
+driver.quit()
